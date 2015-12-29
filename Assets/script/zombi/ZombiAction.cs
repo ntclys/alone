b474643b162db334a53db2d2ae6cf56bb0b2d458
ntclys <ntclys52@gmail.com>
2015-12-29 17:05:09 +0900

    좀비 타격

--- Assets/script/zombi/ZombiAction.cs
+++ Assets/script/zombi/ZombiAction.cs
@@ -6,12 +6,16 @@ public class ZombiAction : MonoBehaviour {
     private ZombiManager zManager;
     private Animator anim;
     private Rigidbody rbody;
+    private Collider col;
+
+    bool isZombiHit = false;
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
         zManager = GetComponent<ZombiManager>();
+        col = GetComponent<Collider>();
     }
 	
 	// Update is called once per frame
@@ -21,6 +25,39 @@ public class ZombiAction : MonoBehaviour {
 
     public void attackPlayer(bool isAttack)
     {
-        anim.SetBool("isZombiAttack", isAttack);
+        if (isZombiHit != false)
+        {
+            anim.SetBool("isZombiAttack", isAttack);
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider Get)
+    {
+        if(Get.gameObject.tag =="Weapon")
+        {
+
+            isZombiHit = true;
+            damageCheck();
+
+        }
+    }
+
+
+    void damageCheck()
+    {
+        zManager.helthPoint -= 20;
+
+        if(zManager.helthPoint > 0)
+        {
+            Debug.Log("hit");
+            anim.SetBool("isZombiAttack", false);
+            anim.SetBool("hit", true);
+        }
+        else
+        {
+            col.isTrigger = true;
+           // Destroy(this.gameObject);
+        }
     }
 }
