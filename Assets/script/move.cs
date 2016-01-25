06ed9877ea82b2d9a928b4f1cd91290c9bef1c58
ntclys <ntclys52@gmail.com>
2016-01-26 00:21:22 +0900

    주인공 움직임 수정

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -8,7 +8,6 @@ public class move : MonoBehaviour {
     private AnimatorStateInfo currentPlayereAni;
     private Rigidbody rbody;
     private Collider playerWeaponCol;
-    private CharacterController charController;
     //[SerializeField] float m_GroundCheckDistance = 0.1f;
 
     public Transform target;
@@ -20,17 +19,21 @@ public class move : MonoBehaviour {
     public float turnSpeed = 10.0f;
     public float gravity = 10.0f;
     private float offsetAngles = 0.0f;
-    private float currentRotation = 0.0f;
-    private float courseNum = 0.0f;
 
     private bool isAttackChk;
     private bool attackMoveOnece;
+
+
+    [SerializeField] private bool m_Jump;
+    [SerializeField] private float m_MaxAngularVelocity = 25; // The maximum velocity the ball can rotate at.
 
 
     private Vector3 movePlayer;
     // the world-relative desired move direction, calculated from the camForward and user input.
 
     private Transform cam; // A reference to the main camera in the scenes transform
+    private Vector3 camForward; // The current forward direction of the camera
+    private bool jump; // whether the jump button is currently pressed
 
 
     enum PlayerState
@@ -59,7 +62,7 @@ public class move : MonoBehaviour {
     {
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
-        charController = GetComponent<CharacterController>();
+        rbody.maxAngularVelocity = m_MaxAngularVelocity;
         playerWeaponCol = playerWeapon.GetComponent<Collider>();
 
         isAttackChk = false;
@@ -70,15 +73,13 @@ public class move : MonoBehaviour {
     {
         currentPlayereAni = anim.GetCurrentAnimatorStateInfo(0);
 
-        if (Input.GetMouseButtonDown(0) && currentPlayereAni.IsName("Attack") == false && charController.isGrounded)
+        if (Input.GetMouseButtonDown(0) && currentPlayereAni.IsName("Attack") == false && m_Jump ==  false)
         {
             isAttackChk = true;
             attackMoveOnece = true;
             anim.SetTrigger("Attack");
             anim.SetBool("isRun", false);
             playerWeaponCol.enabled = true;
-
-            //currentRotation = target.transform.eulerAngles.y;
 
             //Invoke("endAttackAni", 0.1f);  //일정시간후 관련함수 호출
         }
@@ -92,170 +93,108 @@ public class move : MonoBehaviour {
     private void FixedUpdate()
     {
 
-        if (charController.isGrounded == true)
+        if ( currentPlayereAni.IsName("Attack") == false)
         {
-            if (currentPlayereAni.IsName("Attack") == false)
+            if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
             {
-                if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+                anim.SetBool("isRun", true);
+
+                if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
                 {
-                    anim.SetBool("isRun", true);
-                    //transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg, transform.eulerAngles.z);
-                    //rbody.AddForce(this.transform.forward * walkSpeed *Time.deltaTime, ForceMode.Impulse);
 
+                    offsetAngles = 0;
 
-
-                    if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
+                    if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
                     {
-
-                        offsetAngles = 0;
-
-                        if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
-                        {
-                            offsetAngles += 45;
-                        }
-                        else if (CrossPlatformInputManager.GetAxis("Horizontal") < -.1)
-                        {
-                            offsetAngles -= 45;
-                        }
-                        movePlayer = new Vector3(0, movePlayer.y, CrossPlatformInputManager.GetAxis("Vertical"));
-                    }
-                    else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
-                    {
-
-                        offsetAngles = 180;
-
-                        if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
-                        {
-                            offsetAngles -= 45;
-                        }
-                        else if (CrossPlatformInputManager.GetAxis("Horizontal") < -.1)
-                        {
-                            offsetAngles += 45;
-                        }
-
-                        movePlayer = new Vector3(0, movePlayer.y, -CrossPlatformInputManager.GetAxis("Vertical"));
-                    }
-                    else if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
-                    {
-                        offsetAngles = 90;
-
-                        if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
-                        {
-                            offsetAngles += 45;
-                        }
-                        else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
-                        {
-                            offsetAngles -= 45;
-                        }
-
-                        movePlayer = new Vector3(0, movePlayer.y, CrossPlatformInputManager.GetAxis("Horizontal"));
+                        offsetAngles += 45;
                     }
                     else if (CrossPlatformInputManager.GetAxis("Horizontal") < -.1)
                     {
+                        offsetAngles -= 45;
+                    }
+                    movePlayer = new Vector3(0, movePlayer.y, CrossPlatformInputManager.GetAxis("Vertical"));
+                }
+                else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
+                {
 
-                        offsetAngles = 270;
+                    offsetAngles = 180;
 
-                        if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
-                        {
-                            offsetAngles += 45;
-                        }
-                        else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
-                        {
-                            offsetAngles -= 45;
-                        }
-
-                        movePlayer = new Vector3(0, movePlayer.y, -CrossPlatformInputManager.GetAxis("Horizontal"));
+                    if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
+                    {
+                        offsetAngles -= 45;
+                    }
+                    else if (CrossPlatformInputManager.GetAxis("Horizontal") < -.1)
+                    {
+                        offsetAngles += 45;
                     }
 
-                    transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y + offsetAngles, 0);
+                    movePlayer = new Vector3(0, movePlayer.y, -CrossPlatformInputManager.GetAxis("Vertical"));
                 }
-                else
+                else if (CrossPlatformInputManager.GetAxis("Horizontal") > .1)
                 {
-                    anim.SetBool("isRun", false);
-                    rbody.velocity = new Vector3(0, rbody.velocity.y, 0);
-                }               
+                    offsetAngles = 90;
+
+                    if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
+                    {
+                        offsetAngles += 45;
+                    }
+                    else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
+                    {
+                        offsetAngles -= 45;
+                    }
+                    
+                    movePlayer = new Vector3(0, movePlayer.y, CrossPlatformInputManager.GetAxis("Horizontal"));
+                }
+                else if (CrossPlatformInputManager.GetAxis("Horizontal") < -.1)
+                {
+
+                    offsetAngles = 270;
+
+                    if (CrossPlatformInputManager.GetAxis("Vertical") > .1)
+                    {
+                        offsetAngles += 45;
+                    }
+                    else if (CrossPlatformInputManager.GetAxis("Vertical") < -.1)
+                    {
+                        offsetAngles -= 45;
+                    }
+
+                    movePlayer = new Vector3(0, movePlayer.y, -CrossPlatformInputManager.GetAxis("Horizontal"));
+                }
+
+
+                transform.eulerAngles = new Vector3(0f, cam.transform.eulerAngles.y + offsetAngles, 0f);
+                //transform.eulerAngles = new Vector3(0, offsetAngles, 0);
+
+                movePlayer = transform.TransformDirection(movePlayer);
+                //rbody.velocity += movePlayer * walkSpeed * Time.deltaTime;
+                rbody.position += movePlayer  *walkSpeed * Time.deltaTime ;
+                //rbody.AddForce(movePlayer * walkSpeed);
 
             }
             else
             {
-                currentPlayereAni = anim.GetCurrentAnimatorStateInfo(0);
-
-                if (currentPlayereAni.normalizedTime > 0.5f)
-                {
-                    playerWeaponCol.enabled = false;
-                }
-
-
-                if (attackMoveOnece == true)
-                {
-                    Invoke("playerAttackMoveOnce", 0.2f);
-                    attackMoveOnece = false;
-                }
+                anim.SetBool("isRun", false);
+                rbody.velocity = new Vector3(0f, rbody.velocity.y, 0f );
             }
 
-            movePlayer = transform.TransformDirection(movePlayer);
-            movePlayer.y -= gravity * Time.deltaTime;
-            charController.Move(movePlayer * (Time.deltaTime * walkSpeed));
         }
         else
         {
+            currentPlayereAni = anim.GetCurrentAnimatorStateInfo(0);
 
-
-            if (Input.GetAxis("Vertical") > .1)
+            if (currentPlayereAni.normalizedTime > 0.5f)
             {
-                offsetAngles = 0;
-
-                if (Input.GetAxis("Horizontal") > .1)
-                {
-                    offsetAngles += 45;
-                }
-                else if (Input.GetAxis("Horizontal") < -.1)
-                {
-                    offsetAngles -= 45;
-                }
-
-                currentRotation += offsetAngles;
-                courseNum = Input.GetAxis("Vertical");
+                playerWeaponCol.enabled = false;
             }
-            else if (Input.GetAxis("Vertical") < -.1)
+  
+
+            if (attackMoveOnece == true)
             {
-                offsetAngles = 180;
-
-                if (Input.GetAxis("Horizontal") > .1)
-                {
-                    offsetAngles -= 45;
-                }
-                else if (Input.GetAxis("Horizontal") < -.1)
-                {
-                    offsetAngles += 45;
-                }
-
-                currentRotation +=  offsetAngles;
-                courseNum = -Input.GetAxis("Vertical");
+                Invoke("playerAttackMoveOnce", 0.2f);
+                attackMoveOnece = false;
             }
-            else if (Input.GetAxis("Horizontal") > .1)
-            {
-                currentRotation  += 90;
-                courseNum = movePlayer.z + Input.GetAxis("Horizontal");
-            }
-            else if (Input.GetAxis("Horizontal") < -.1)
-            {
-                currentRotation += 270;
-                courseNum = -Input.GetAxis("Horizontal");
-            }
-            else
-            {
-                movePlayer.y += 0.03f;
-            }
-
-            transform.eulerAngles = new Vector3(0, currentRotation, 0);
-            movePlayer = new Vector3(0, movePlayer.y, courseNum);
-
         }
-
-        movePlayer = transform.TransformDirection(movePlayer);
-        movePlayer.y -= gravity * Time.deltaTime;
-        charController.Move(movePlayer * (Time.deltaTime * walkSpeed));
     }
 
 
@@ -272,7 +211,8 @@ public class move : MonoBehaviour {
 
     private void playerAttackMoveOnce()
     {
-        rbody.AddForce(this.transform.forward * walkSpeed * Time.deltaTime, ForceMode.Impulse);
+        //rbody.AddForce(this.transform.forward * walkSpeed * Time.deltaTime, ForceMode.Impulse);
+        rbody.velocity = transform.forward *2;
     }
 
     void HandleGroundedMovement(bool dodge, bool jump)
@@ -283,18 +223,32 @@ public class move : MonoBehaviour {
             // jump!
             Debug.Log("jump");
             
-            if (charController.isGrounded )
+            if (m_Jump == false)
             {
+                m_Jump = true;
                 anim.SetTrigger("jump");
                 anim.applyRootMotion = false;
-                currentRotation = cam.transform.eulerAngles.y;
-                //rbody.velocity = new Vector3(rbody.velocity.x, jumpPower, rbody.velocity.z);
-                movePlayer.y = jumpPower;
+                rbody.velocity = new Vector3(rbody.velocity.x, jumpPower, rbody.velocity.z);
             }
-            
+            Debug.Log("Jump"+ m_Jump);
+
         }
     }
 
+
+    void OnCollisionStay()
+    {
+        currentPlayereAni = anim.GetCurrentAnimatorStateInfo(0);
+        //Debug.Log(currentPlayereAni.normalizedTime);
+        if (m_Jump )
+        {
+            m_Jump = false;
+            Debug.Log("!!"+ currentPlayereAni.normalizedTime);
+            //anim.SetBool("isJump", false);
+            //anim.SetTrigger("land");
+        }
+        //Debug.Log("touch");
+    }
 
 
     /*
